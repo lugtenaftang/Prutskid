306a4cc164fdf15e5fbbb2823e608941b0ed5bca
Your Name <you@example.com>
2015-03-07 19:58:09 +0100

    Tilbage igen

--- Assets/Scripts/CameraZoom.cs
+++ Assets/Scripts/CameraZoom.cs
@@ -15,8 +15,8 @@ public class CameraZoom : MonoBehaviour
 	private float ballVelocityX;
 	private float ballVelocityY;
 	private float ballVelocity;
-	public float smoothingPole = 0.9f;
-	private Vector3 filter;
+//	public float smoothingPole = 0.9f;
+//	private Vector3 filter;
 
 	void Start () 
 	{
@@ -25,39 +25,39 @@ public class CameraZoom : MonoBehaviour
 	
 	void Update () 
 	{
-//		transform.position = Vector3.SmoothDamp (transform.position, cameraPoint.transform.position, ref camVelocity, smoothTime / 8f);
-//
-//		ballVelocityX = playah.rigidbody2D.velocity.x;
-//		ballVelocityY = playah.rigidbody2D.velocity.y;
-//
-//		if (ballVelocityX < 0f) 
-//		{
-//			ballVelocityX = ballVelocityX * (-1);
-//		}
-//		if (ballVelocityY < 0f) 
-//		{
-//			ballVelocityY = ballVelocityY * (-1);
-//		}
-//		ballVelocity = ballVelocityX + ballVelocityY; 
-//
-//		float zoomTarget = ballVelocity / 3;
-//		camera.orthographicSize = Mathf.SmoothDamp(camera.orthographicSize, zoomTarget, ref yVelocity, smoothTime);
-//
-//		if (camera.orthographicSize < 4f) 
-//		{
-//			camera.orthographicSize = 4f;
-//		}
-//		if (camera.orthographicSize > 7f) 
-//		{
-//			camera.orthographicSize = 7f;
-//		}
-//
-//		camZoomText.text = "Camera Zoom  (please ignore): " + camera.orthographicSize.ToString("0.##");
+		transform.position = Vector3.SmoothDamp (transform.position, cameraPoint.transform.position, ref camVelocity, smoothTime / 8f);
 
-		float newX = playah.transform.position.x + smoothingPole * (transform.position.x - playah.transform.position.x);
-		float newY = playah.transform.position.y + smoothingPole * (transform.position.y - playah.transform.position.y);
-		
-		transform.position = new Vector3(newX, newY, transform.position.z);
+		ballVelocityX = playah.rigidbody2D.velocity.x;
+		ballVelocityY = playah.rigidbody2D.velocity.y;
+
+		if (ballVelocityX < 0f) 
+		{
+			ballVelocityX = ballVelocityX * (-1);
+		}
+		if (ballVelocityY < 0f) 
+		{
+			ballVelocityY = ballVelocityY * (-1);
+		}
+		ballVelocity = ballVelocityX + ballVelocityY; 
+
+		float zoomTarget = ballVelocity / 3;
+		camera.orthographicSize = Mathf.SmoothDamp(camera.orthographicSize, zoomTarget, ref yVelocity, smoothTime);
+
+		if (camera.orthographicSize < 4f) 
+		{
+			camera.orthographicSize = 4f;
+		}
+		if (camera.orthographicSize > 7f) 
+		{
+			camera.orthographicSize = 7f;
+		}
+
+		camZoomText.text = "Camera Zoom  (please ignore): " + camera.orthographicSize.ToString("0.##");
+
+//		float newX = playah.transform.position.x + smoothingPole * (transform.position.x - playah.transform.position.x);
+//		float newY = playah.transform.position.y + smoothingPole * (transform.position.y - playah.transform.position.y);
+//		
+//		transform.position = new Vector3(newX, newY, transform.position.z);
 		
 	}
 }
