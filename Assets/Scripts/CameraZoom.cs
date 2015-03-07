edec50ecd916276641420cd592ba525e2c40e2a0
Your Name <you@example.com>
2015-03-07 14:50:40 +0100

    janus shit low pass filter

--- Assets/Scripts/CameraZoom.cs
+++ Assets/Scripts/CameraZoom.cs
@@ -9,50 +9,55 @@ public class CameraZoom : MonoBehaviour
 
 	public UnityEngine.UI.Text camZoomText; 
 
-	public float smoothTime = 5f;
+	public float smoothTime = 1.5f;
 	private float yVelocity = 0f;
 	private Vector3 camVelocity = Vector3.zero;
 	private float ballVelocityX;
 	private float ballVelocityY;
 	private float ballVelocity;
-
+	public float smoothingPole = 0.9f;
+	private Vector3 filter;
 
 	void Start () 
 	{
-		
+
 	}
 	
 	void Update () 
 	{
-		transform.position = Vector3.SmoothDamp (transform.position, cameraPoint.transform.position, ref camVelocity, smoothTime / 8f);
+//		transform.position = Vector3.SmoothDamp (transform.position, cameraPoint.transform.position, ref camVelocity, smoothTime / 8f);
+//
+//		ballVelocityX = playah.rigidbody2D.velocity.x;
+//		ballVelocityY = playah.rigidbody2D.velocity.y;
+//
+//		if (ballVelocityX < 0f) 
+//		{
+//			ballVelocityX = ballVelocityX * (-1);
+//		}
+//		if (ballVelocityY < 0f) 
+//		{
+//			ballVelocityY = ballVelocityY * (-1);
+//		}
+//		ballVelocity = ballVelocityX + ballVelocityY; 
+//
+//		float zoomTarget = ballVelocity / 3;
+//		camera.orthographicSize = Mathf.SmoothDamp(camera.orthographicSize, zoomTarget, ref yVelocity, smoothTime);
+//
+//		if (camera.orthographicSize < 4f) 
+//		{
+//			camera.orthographicSize = 4f;
+//		}
+//		if (camera.orthographicSize > 7f) 
+//		{
+//			camera.orthographicSize = 7f;
+//		}
+//
+//		camZoomText.text = "Camera Zoom  (please ignore): " + camera.orthographicSize.ToString("0.##");
 
-
-
-		ballVelocityX = playah.rigidbody2D.velocity.x;
-		ballVelocityY = playah.rigidbody2D.velocity.y;
-
-		if (ballVelocityX < 0f) 
-		{
-			ballVelocityX = ballVelocityX * (-1);
-		}
-		if (ballVelocityY < 0f) 
-		{
-			ballVelocityY = ballVelocityY * (-1);
-		}
-		ballVelocity = ballVelocityX + ballVelocityY; 
-
-		float zoomTarget = ballVelocity / 3;
-		camera.orthographicSize = Mathf.SmoothDamp(camera.orthographicSize, zoomTarget, ref yVelocity, smoothTime);
-
-		if (camera.orthographicSize < 4f) 
-		{
-			camera.orthographicSize = 4f;
-		}
-		if (camera.orthographicSize > 7f) 
-		{
-			camera.orthographicSize = 7f;
-		}
-
-		camZoomText.text = "Camera Zoom  (please ignore): " + camera.orthographicSize.ToString("0.##");
+		float newX = playah.transform.position.x + smoothingPole * (transform.position.x - playah.transform.position.x);
+		float newY = playah.transform.position.y + smoothingPole * (transform.position.y - playah.transform.position.y);
+		
+		transform.position = new Vector3(newX, newY, transform.position.z);
+		
 	}
 }
